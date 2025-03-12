c93a64df67ae764240ee69cb25fe106f0094cfd5
jafar fuad <jata1518@gmail.com>
2025-03-12 17:01:09 +0200

    Added IBookingRepository interface with Service Folder

---
+++ Airport_Ticket_Booking/interfaces/IBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using Airport_Ticket_Booking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.interfaces
+{
+    public interface IBookingRepository
+    {
+        public void SaveBookings(List<Booking> bookings);
+        public List<Booking> GetAllBookings();
+    }
+}
