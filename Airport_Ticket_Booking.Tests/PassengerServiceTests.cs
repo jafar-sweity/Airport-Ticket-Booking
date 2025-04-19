db4f0e060171b8b3743327fbf084ebe31c191d99
jafar fuad <jata1518@gmail.com>
2025-04-19 20:54:53 +0300

    Test PassengerService Methods ; Add unit test for BookFlight in PassengerService

---
+++ Airport_Ticket_Booking.Tests/PassengerServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Airport_Ticket_Booking.interfaces;
+using Airport_Ticket_Booking.models.Enums;
+using Airport_Ticket_Booking.models;
+using Airport_Ticket_Booking.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Airport_Ticket_Booking.models;
+using Airport_Ticket_Booking.models.Enums;
+using Airport_Ticket_Booking.Services;
+using Airport_Ticket_Booking.interfaces;
+
+namespace Airport_Ticket_Booking.Tests
+{
+    public class PassengerServiceTests
+    {
+        private readonly Mock<IBookingService> _mockBookingService;
+        private readonly Mock<IFlightRepository> _mockFlightRepository;
+        private readonly PassengerService _passengerService;
+
+        public PassengerServiceTests()
+        {
+            _mockBookingService = new Mock<IBookingService>();
+            _mockFlightRepository = new Mock<IFlightRepository>();
+            _passengerService = new PassengerService(_mockBookingService.Object, _mockFlightRepository.Object);
+        }
+
+        [Fact]
+        public void BookFlight_ShouldCallBookingServiceWithCorrectBooking()
+        {
+            var passenger = new Passenger { ID = 1 };
+            var flight = new Flight { FlightNumber = 101, BasePrice = 200 };
+            var flightClass = FlightClass.Business;
+
+            _passengerService.BookFlight(passenger, flight, flightClass);
+
+            _mockBookingService.Verify(bs => bs.BookFlight(It.Is<Booking>(
+                b => b.PassengerId == passenger.ID &&
+                     b.Flight == flight.FlightNumber &&
+                     b.FlightClass == flightClass &&
+                     b.Price == flight.BasePrice * 1.5m
+            )), Times.Once);
+        }
+
+    }
+}
